7c7eeb3798c2adfed0b724636d5ca550cd62304f
IraTokar <tokar.ira20@gmail.com>
2024-04-23 14:02:28 +0200

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+
+buttonStop.disabled = true;
+
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+
+buttonStart.addEventListener('click', () => {
+    // body.style.backgroundColor = getRandomHexColor();
+    buttonStart.disabled = true;
+    buttonStop.disabled = false;
+    timerId = setInterval(onButtonStart, 1000);
+});
+
+function onButtonStart() {
+    body.style.backgroundColor = getRandomHexColor();
+};
+
+buttonStop.addEventListener('click', () => {
+    clearInterval(timerId);
+    buttonStart.disabled = false;
+    buttonStop.disabled = true;
+})
+
+
+
 
